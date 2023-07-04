a36c9aec2228c85c222b8d0c1ef051a5a2fa2200
Xardas0327 <32038221+Xardas0327@users.noreply.github.com>
2023-07-04 17:42:03 +0100

    ADD SettingsInspector

---
+++ Assets/ML-Agents Configuration/Configuration/Inspector/SettingsInspector.cs
@@ -0,0 +1,55 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Xardas.MLAgents.Configuration.Inspector
+{
+    public abstract class SettingsInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawInspector();
+        }
+
+        void DrawInspector()
+        {
+            using (new LocalizationGroup(target))
+            {
+                EditorGUI.BeginChangeCheck();
+                serializedObject.UpdateIfRequiredOrScript();
+                SerializedProperty iterator = serializedObject.GetIterator();
+                bool enterChildren = true;
+                while (iterator.NextVisible(enterChildren))
+                {
+                    using (new EditorGUI.DisabledScope("m_Script" == iterator.propertyPath))
+                    {
+                        if (iterator.displayName == "Script")
+                        {
+                            EditorGUILayout.PropertyField(iterator, true);
+                            EditorGUILayout.Space();
+                        }
+                        else
+                            DrawProperty(iterator);
+                    }
+                }
+
+                serializedObject.ApplyModifiedProperties();
+                EditorGUI.EndChangeCheck();
+            }
+        }
+
+        protected abstract void DrawProperty(SerializedProperty property);
+
+        protected void DrawPropertyWithTickBox(ref bool active, SerializedProperty property)
+        {
+            EditorGUILayout.BeginHorizontal();
+            active = EditorGUILayout.Toggle(active, GUILayout.MaxWidth(15));
+            EditorGUI.BeginDisabledGroup(!active);
+            EditorGUILayout.PropertyField(property, true);
+            EditorGUI.EndDisabledGroup();
+            EditorGUILayout.EndHorizontal();
+        }
+
+    }
+}
+#endif
--- Assets/ML-Agents Configuration/Configuration/Inspector/TorchSettingsInspector.cs
+++ Assets/ML-Agents Configuration/Configuration/Inspector/TorchSettingsInspector.cs
@@ -6,44 +6,14 @@ using Xardas.MLAgents.Configuration.Fileformat;
 namespace Xardas.MLAgents.Configuration.Inspector
 {
     [CustomEditor(typeof(TorchSettings))]
-    public class TorchSettingsInspector : Editor
+    public class TorchSettingsInspector : SettingsInspector
     {
-        public override void OnInspectorGUI()
+        protected override void DrawProperty(SerializedProperty property)
         {
-            DrawIspector((TorchSettings)target);
-        }
-
-        void DrawIspector(TorchSettings settings)
-        {
-            using (new LocalizationGroup(target))
+            TorchSettings settings = (TorchSettings)target;
+            if (property.name == nameof(settings.torchSettings.device))
             {
-                EditorGUI.BeginChangeCheck();
-                serializedObject.UpdateIfRequiredOrScript();
-                SerializedProperty iterator = serializedObject.GetIterator();
-                bool enterChildren = true;
-                while (iterator.NextVisible(enterChildren))
-                {
-                    using (new EditorGUI.DisabledScope("m_Script" == iterator.propertyPath))
-                    {
-                        if (iterator.displayName == "Script")
-                        {
-                            EditorGUILayout.PropertyField(iterator, true);
-                            EditorGUILayout.Space();
-                        }
-                        else if(iterator.name == nameof(settings.torchSettings.device))
-                        {
-                            EditorGUILayout.BeginHorizontal();
-                            settings.torchSettings.isUseDevice = EditorGUILayout.Toggle(settings.torchSettings.isUseDevice, GUILayout.MaxWidth(15));
-                            EditorGUI.BeginDisabledGroup(!settings.torchSettings.isUseDevice);
-                            EditorGUILayout.PropertyField(iterator, true);
-                            EditorGUI.EndDisabledGroup();
-                            EditorGUILayout.EndHorizontal();
-                        } 
-                    }
-                }
-
-                serializedObject.ApplyModifiedProperties();
-                EditorGUI.EndChangeCheck();
+                DrawPropertyWithTickBox(ref settings.torchSettings.isUseDevice, property);
             }
         }
     }
